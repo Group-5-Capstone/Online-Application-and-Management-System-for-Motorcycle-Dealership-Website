505850df6cbe6dedbddb3a90baede2cedecc6529
Jssmrnd <miranda.133778@balayan.sti.edu.ph>
2023-07-17 01:31:48 +0800

    updated with reg form

--- javascript/script.js
+++ javascript/script.js
@@ -29,3 +29,38 @@ window.addEventListener("scroll", function () {
   prevScrollPos = currentScrollPos;
 });
 
+//For Password Eye
+const eyeIcons = document.querySelectorAll(".eye-icon");
+eyeIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const passwordInput = icon.previousElementSibling;
+    const passwordInputType = passwordInput.getAttribute("type");
+    if (passwordInputType === "password") {
+      passwordInput.setAttribute("type", "text");
+      icon.classList.remove("ri-eye-close-line");
+      icon.classList.add("ri-eye-line");
+    } else {
+      passwordInput.setAttribute("type", "password");
+      icon.classList.remove("ri-eye-line");
+      icon.classList.add("ri-eye-close-line");
+    }
+    passwordInput.focus();
+  });
+});
+
+
+//For Registration Form
+function toggleRegisterForm() {
+  var content2 = document.getElementById("Content2");
+  content2.style.transform = "translateX(0%)"; /* Slide in from the right */
+  content2.style.zIndex = 2;
+  content2.style.right = "0%";
+  content2.style.transition = "all 0.6s ease-in-out";
+}
+
+function closeForm() {
+  var content2 = document.getElementById("Content2");
+  content2.style.transform = "translateX(-100%)";
+  content2.style.zIndex = 2;
+  content2.style.transition = "all 0.6s ease-in-out";
+}
